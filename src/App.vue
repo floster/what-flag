251f8c2a0ddd4657eae167fa411bedecd2262c91
mr.valerio <valera.osadchuk@gmail.com>
2024-05-27 09:36:43 +0300

    project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const hello = ref('Hello, Vue 3 + Vite + Tailwindcss + DaisyUI!')
+</script>
+
+<template>
+  <h1 class="heading-1">{{ hello }}</h1>
+</template>
